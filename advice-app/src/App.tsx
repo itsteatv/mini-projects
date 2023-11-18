9af2fea1ac1c73b4c80dfd4610aa678b915375e6
itsteatv <rezashow60@gmail.com>
2023-11-18 06:26:23 +0330

    react query added

--- advice-app/src/App.tsx
+++ advice-app/src/App.tsx
@@ -1,10 +1,27 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // staleTime: 60 * 1000,
+        staleTime: 0,
+      },
+    },
+  });
   return (
-    <div>
-      
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
