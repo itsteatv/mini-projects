5c927ab9429c4db5e0043515a2aab5430f4c2635
itsteatv <rezashow60@gmail.com>
2023-11-19 05:07:46 +0330

    useAdvice hook updated

--- advice-app/src/hooks/useAdvice.tsx
+++ advice-app/src/hooks/useAdvice.tsx
@@ -1,22 +1,34 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 interface Advice {
-    id: number;
-    advice: string;
+  id: number;
+  advice: string;
 }
 
 const fetchAdvice = async (): Promise<Advice> => {
-    const response = await fetch('https://api.adviceslip.com/advice');
-    const data = await response.json();
-    console.log(data);
-    return data.slip;
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  console.log(data);
+  return data.slip;
 };
 
 export function useAdvice() {
-    const { isLoading, data: advice, error } = useQuery({
-        queryKey: ['advice'],
-        queryFn: fetchAdvice,
-    });
+  const queryClient = useQueryClient();
 
-    return { isLoading, advice, error }
+  const {
+    isLoading,
+    data: advice,
+    error,
+  } = useQuery({
+    queryKey: ["advice"],
+    queryFn: fetchAdvice,
+  });
+
+  const refetch = () => {
+    queryClient.invalidateQueries({
+      queryKey: ["advice"],
+    });
+  };
+
+  return { isLoading, advice, error, refetch };
 }
