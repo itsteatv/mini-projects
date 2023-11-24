484063d3a4a8e931eff278efd57fe0abc74440a7
itsteatv <rezashow60@gmail.com>
2023-11-24 20:24:18 +0330

    writing a simple layout

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -1,10 +1,15 @@
-
 function App() {
   return (
-    <div>
-      
+    <div className="flex flex-row h-screen overflow-hidden touch-pan-y touch-pan-x">
+      <div className="h-screen w-[15rem] flex flex-col border-r-slate-50">
+        sidebar
+      </div>
+      <div className="h-screen flex flex-col flex-grow">
+        <div className="flex items-center border-r-slate-50">Toolbar</div>
+        Text editor
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
