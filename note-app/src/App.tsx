5bde6bb957b6fa1c879f2534106a7b9ea4224027
itsteatv <rezashow60@gmail.com>
2023-11-24 21:23:25 +0330

    routing added

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -1,14 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="flex flex-row h-screen overflow-hidden touch-pan-y touch-pan-x">
-      <div className="h-screen w-[15rem] flex flex-col border-r-slate-50">
-        sidebar
-      </div>
-      <div className="h-screen flex flex-col flex-grow">
-        <div className="flex items-center border-r-slate-50">Toolbar</div>
-        Text editor
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route element={<ProtectedRoute> <AppLayout /> </ProtectedRoute>}> */}
+        <Route index path="*" element={<Navigate replace to="/" />} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="/edit" element={<h1>Edit</h1>} />
+        </Route>
+        {/* </Route> */}
+
+        {/* <Route path="*" element={<PageNotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
