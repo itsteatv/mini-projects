56fca18f92677eebfa11d5f78e72f1695723f2fd
itsteatv <rezashow60@gmail.com>
2023-11-24 23:43:22 +0330

    small bug removed

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Route path="/new" element={<h1>New</h1>} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
-          <Route path="/edit" element={<h1>Edit</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
         </Route>
         {/* </Route> */}
 
