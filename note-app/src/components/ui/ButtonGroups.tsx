2b04e72cf862354373233199a65849e0cfc1450e
itsteatv <rezashow60@gmail.com>
2023-11-26 02:35:58 +0330

    ui folder with new component added

---
+++ note-app/src/components/ui/ButtonGroups.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
+export default function ButtonGroups() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className=">=345px:flex-col flex gap-4 justify-end">
+        <button
+          type="button"
+          className="hover:shadow-form rounded-md bg-[#6A64F1] hover:bg-[#4f46e5] transition py-3 px-8 text-base font-semibold text-white outline-none"
+        >
+          Submit
+        </button>
+        <button
+          type="reset"
+          onClick={() => navigate("..")}
+          className="hover:shadow-form rounded-md bg-[#64748b] hover:bg-[#475569] transition py-3 px-8 text-base font-semibold text-white outline-none"
+        >
+          Cancel
+        </button>
+      </div>
+    </>
+  );
+}
