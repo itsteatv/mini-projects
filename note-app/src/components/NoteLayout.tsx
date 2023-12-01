72532e77e86bfdd1242cbbcc09387059bc741ed9
itsteatv <rezashow60@gmail.com>
2023-12-01 16:40:19 +0330

    loggin note data

--- note-app/src/components/NoteLayout.tsx
+++ note-app/src/components/NoteLayout.tsx
@@ -10,7 +10,13 @@ export function NoteLayout({ notes }: NoteLayoutProps) {
   const { id } = useParams();
 
   const note = notes.find((note) => note.id === id);
-  console.log(id, note);
+  console.log(`
+  ID => ${id}
+  -----------
+  NOTE ID => ${note?.id}
+  NOTE TITLE => ${note?.title}
+  NOTE MARKDOWN => ${note?.markdown}
+  `);
 
   if (note === null) return <PageNotFound />;
 
