5f2b0abc83a5ab1f3ce4fd48619927fecddf2c1a
itsteatv <rezashow60@gmail.com>
2023-12-01 15:57:37 +0330

    note layout component updated

--- note-app/src/components/NoteLayout.tsx
+++ note-app/src/components/NoteLayout.tsx
@@ -1,5 +1,18 @@
-function NoteLayout() {
-  return <div></div>;
-}
+import { Note } from "./types/types";
+import { Outlet, useParams } from "react-router-dom";
+import PageNotFound from "./ui/PageNotFound";
 
-export default NoteLayout;
+type NoteLayoutProps = {
+  notes: Note[];
+};
+
+export function NoteLayout({ notes }: NoteLayoutProps) {
+  const { id } = useParams();
+
+  const note = notes.find((note) => note.id === id);
+  console.log(id, note);
+
+  if (note === null) return <PageNotFound />;
+
+  return <Outlet context={note} />;
+}
