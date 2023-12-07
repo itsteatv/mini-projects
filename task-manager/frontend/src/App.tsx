35264947d4fca26a84d9f0e3e8c47dad0573088a
itsteatv <rezashow60@gmail.com>
2023-12-07 19:00:53 +0330

    react query setup

--- task-manager/frontend/src/App.tsx
+++ task-manager/frontend/src/App.tsx
@@ -1,46 +1,19 @@
-import { useState, useEffect } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
-  interface Task {
-    _id: number;
-    title: string;
-    desc: string;
-    completed: boolean;
-  }
-
-  const [tasks, setTasks] = useState<Task[]>([]);
-  console.log(tasks);
-
-  const url = "http://localhost:5000";
-
-  useEffect(() => {
-    const fetchTask = async () => {
-      try {
-        const response = await fetch(`${url}/api/tasks`);
-        console.log(response);
-
-        const data: Task[] = await response.json();
-        console.log(data);
-        setTasks(data);
-      } catch (error) {
-        console.error("Error fetching data:" + error);
-      }
-    };
-
-    fetchTask();
-  }, []);
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
 
   return (
-    <>
-      <h1>Task:</h1>
-      {tasks.map((task) => (
-        <div className="text-black flex flex-col" key={task._id}>
-          <p>TITLE: {task.title}</p>
-          <p>DESC: {task.desc}</p>
-          <p>STATUS: {task.completed.toString()}</p>
-        </div>
-      ))}
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
