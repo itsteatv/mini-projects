45fe322083079364bd742ff0e4dc353dde4a0a3c
itsteatv <rezashow60@gmail.com>
2023-12-07 03:32:38 +0330

    note component updated

--- note-app/src/components/Note.tsx
+++ note-app/src/components/Note.tsx
@@ -14,44 +14,52 @@ export function Note({ handleDeleteNote }: NoteProps) {
   console.log(note);
 
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-      <ButtonGroups
-        handleDeleteNote={handleDeleteNote}
-        id={note.id}
-        firstButton={{
-          label: "Edit",
-          onClick: () => navigate(`/${note?.id}/edit`),
-          className: "font-Ubuntu btn btn-primary dark:text-white",
-        }}
-        secButton={{
-          label: "Delete",
-          className: "font-Ubuntu btn btn-error dark:text-white",
-        }}
-        thirdButton={{
-          label: "Back",
-          onClick: () => navigate("/"),
-          className: "font-Ubuntu btn btn-active btn-ghost dark:text-white",
-        }}
-      />
-      <Markdown className="mt-5">{note?.markdown}</Markdown>
-      <div className="text-2xl leading-snug font-extrabold text-black truncate mb-1 sm:mb-0">
-        ID: {note?.id}
+    <>
+      <div className="min-h-screen flex items-center justify-center text-center text-white font-Ubuntu">
+        <figure>
+          <blockquote className="p-6 text-lg leading-relaxed">
+            <cite className="text-left not-italic">
+              <div className="flex justify-between >=345px:flex-col >=345px:items-center ml-1 text-white font-bold text-2xl">
+                {note?.title}
+                <span className="font-bold uppercase">
+                  {note?.tags?.length > 0 &&
+                    note?.tags.map((tag) => (
+                      <span
+                        className="font-bold uppercase badge badge-outline"
+                        key={tag?.id}
+                      >
+                        {tag?.label}
+                      </span>
+                    ))}
+                </span>
+              </div>
+            </cite>
+            <Markdown className="mt-10 text-left">{note?.markdown}</Markdown>
+          </blockquote>
+          <figcaption className="p-6 pt-2 text-sm text-sky-500">
+            <div className="flex flex-col justify-center gap-1 mx-auto"></div>
+            <ButtonGroups
+              handleDeleteNote={handleDeleteNote}
+              id={note.id}
+              firstButton={{
+                label: "Edit",
+                onClick: () => navigate(`/${note?.id}/edit`),
+                className: "font-Ubuntu btn btn-primary dark:text-white",
+              }}
+              secButton={{
+                label: "Delete",
+                className: "font-Ubuntu btn btn-error dark:text-white",
+              }}
+              thirdButton={{
+                label: "Back",
+                onClick: () => navigate("/"),
+                className:
+                  "font-Ubuntu btn btn-active btn-ghost dark:text-white",
+              }}
+            />
+          </figcaption>
+        </figure>
       </div>
-      <div className="text-2xl leading-snug font-extrabold text-black truncate mb-1 sm:mb-0">
-        TITLE: {note?.title}
-      </div>
-      <div className="text-2xl leading-snug font-extrabold text-black truncate mb-1 sm:mb-0">
-        MARKDOWN TEXT: {note?.markdown}
-      </div>
-      {note?.tags?.length > 0 &&
-        note?.tags.map((tag) => (
-          <h2
-            className="text-2xl leading-snug font-extrabold text-black truncate mb-1 sm:mb-0"
-            key={tag?.id}
-          >
-            TAG LABEL: {tag?.label}
-          </h2>
-        ))}
-    </div>
+    </>
   );
 }
