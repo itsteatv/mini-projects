981485cf3dac92b8a71657fa0f749bf1ba6f9aa0
itsteatv <rezashow60@gmail.com>
2023-12-08 13:21:59 +0330

    spinner component added

---
+++ task-manager/frontend/src/components/ui/Spinner.tsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
+  );
+}
+
+export default Spinner;
