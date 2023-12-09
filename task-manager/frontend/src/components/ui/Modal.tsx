a8360bdc238b28e1f0a95a28074a476b2192f486
itsteatv <rezashow60@gmail.com>
2023-12-09 08:33:57 +0330

    adding new task ui in modal

--- task-manager/frontend/src/components/ui/Modal.tsx
+++ task-manager/frontend/src/components/ui/Modal.tsx
@@ -1,8 +1,28 @@
+import { Task } from "../types/Types";
+import { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
 interface ModalProps {
   closeModal: () => void;
+  addTask: (newTask: Task) => void;
 }
 
-export default function Modal({ closeModal }: ModalProps) {
+export default function Modal({ closeModal, addTask }: ModalProps) {
+  const [title, setTitle] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const [completed, setCompleted] = useState<boolean>(false);
+
+  const handleAddTask = () => {
+    const newTask: Task = {
+      _id: uuidV4(),
+      title,
+      desc,
+      completed,
+    };
+
+    addTask(newTask);
+    closeModal();
+  };
+
   return (
     <>
       <dialog id="my_modal_3" className="modal">
@@ -15,8 +35,46 @@ export default function Modal({ closeModal }: ModalProps) {
               ✕
             </button>
           </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+          <form className="flex flex-col items-center">
+            <h2 className="text-left font-bold font-Ubuntu">Add Task</h2>
+            <div className="mt-4">
+              <input
+                type="text"
+                value={title}
+                placeholder="Type title here"
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="mt-4">
+              <input
+                type="text"
+                value={desc}
+                placeholder="Type description here"
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setDesc(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-4">
+              <label className="label cursor-pointer">
+                <span className="label-text">Completed ?</span>
+                <input
+                  checked={completed}
+                  onChange={(e) => setCompleted(e.target.checked)}
+                  type="checkbox"
+                  className="checkbox ml-4"
+                />
+              </label>
+            </div>
+            <div className="flex gap-4 mt-4">
+              <button className="btn btn-success" onClick={handleAddTask}>
+                Add Task
+              </button>
+              <button className="btn btn-warning" onClick={closeModal}>
+                Cancel
+              </button>
+            </div>
+          </form>
         </div>
       </dialog>
     </>
