8d600d770712d3e287b88faae42e097ee47ee5f8
itsteatv <rezashow60@gmail.com>
2023-12-09 07:46:44 +0330

    dropdown component added + connection with modal

---
+++ task-manager/frontend/src/components/ui/Dropdown.tsx
@@ -0,0 +1,50 @@
+import { CgTrashEmpty, CgPen, CgAdd, CgMoreVerticalAlt } from "react-icons/cg";
+import { useState, useRef, useEffect } from "react";
+import Modal from "./Modal";
+
+export default function Dropdown() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const modalElement = document.getElementById("my_modal_3");
+
+    if (isModalOpen && modalElement) {
+      modalElement.showModal();
+    }
+  }, [isModalOpen]);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <div className="dropdown dropdown-end">
+        <div className="text-black">
+          <CgMoreVerticalAlt tabIndex={0} role="button" />
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] mt-[0.25rem] menu p-2 shadow bg-white rounded-box"
+        >
+          <div className="flex gap-2 text-black cursor-pointer">
+            <CgTrashEmpty />
+            <CgAdd
+              onClick={() => {
+                setIsModalOpen(true);
+              }}
+            />
+            <CgPen />
+          </div>
+        </ul>
+      </div>
+
+      {/* Modal Component */}
+      {isModalOpen && <Modal closeModal={closeModal} />}
+    </>
+  );
+}
