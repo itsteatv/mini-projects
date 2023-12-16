db4f478d8fccdf59264517a793a012791947bf23
itsteatv <rezashow60@gmail.com>
2023-12-17 01:47:22 +0330

    react router dom setup + main components

--- jwt-auhtentication/client/src/App.tsx
+++ jwt-auhtentication/client/src/App.tsx
@@ -1,5 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginForm from "./components/ui/Login";
+import RegisterForm from "./components/ui/Register";
+import PageNotFound from "./components/ui/PageNotFound";
 function App() {
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -12,6 +16,14 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="login" />} />
+          <Route path="login" element={<LoginForm />} />
+          <Route path="register" element={<RegisterForm />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
---
+++ jwt-auhtentication/client/src/components/ui/Register.tsx
@@ -0,0 +1,5 @@
+function RegisterForm() {
+  return <div></div>;
+}
+
+export default RegisterForm;
