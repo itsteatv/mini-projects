8e8d12fd5496f9ce39c2d72291fd575b119f505a
itsteatv <rezashow60@gmail.com>
2023-12-16 22:32:59 +0330

    react query setup

--- jwt-auhtentication/client/src/App.tsx
+++ jwt-auhtentication/client/src/App.tsx
@@ -1,10 +1,19 @@
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+        refetchOnReconnect: true,
+      },
+    },
+  });
   return (
-    <div>
-      
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
