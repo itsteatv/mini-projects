8e29a7ff6c81d2ce062cbb69ab4a243acc67308a
itsteatv <rezashow60@gmail.com>
2023-12-19 17:14:14 +0330

    new page when you're logged in

--- jwt-authentication/client/src/components/hooks/useLogin.tsx
+++ jwt-authentication/client/src/components/hooks/useLogin.tsx
@@ -2,8 +2,11 @@ import toast from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
 import { FormsData } from "../utils/types";
 import { LoginAPI } from "../api/LoginAPI";
+import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
+  const navigate = useNavigate();
+
   const { isPending, mutate: login } = useMutation({
     mutationFn: ({ email, password }: FormsData) =>
       LoginAPI({ email, password }),
@@ -12,7 +15,11 @@ export function useLogin() {
       toast.success("Success! You are logged in.");
 
       const token = data.token;
-      localStorage.setItem("token", token);
+
+      if (token) {
+        localStorage.setItem("token", token);
+        navigate("/welcome");
+      }
 
       console.log(data);
     },
--- jwt-authentication/client/src/components/pages/Welcome.tsx
+++ jwt-authentication/client/src/components/pages/Welcome.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../ui/Spinner";
+
 function Welcome() {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      navigate("/login");
+    }
+    setLoading(false);
+  }, [navigate]);
+
+  if (loading) {
+    return <Spinner spinnerType="infinity" size="lg" />;
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen font-IBM text-5xl uppercase">
+      Welcome
+    </div>
+  );
 }
 
 export default Welcome;
